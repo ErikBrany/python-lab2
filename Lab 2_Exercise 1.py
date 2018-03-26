396f9848cc23b6e35b0bbd3ba3c3d50528d19d80
Erik Branyon <s254671@studenti.polito.it>
2018-03-26 20:14:04 +0200

    Lab 2 exercise 1 done

--- Lab 2_Exercise 1.py
+++ Lab 2_Exercise 1.py
@@ -1,8 +1,8 @@
 #Exercise 1
-task_list = ["Call John for AmI project", "Buy a new mouse","Find a present for Anelina's birthday", "Organize mega party (last week of April)", "Book summer Holidays", "WhatsApp Mary"]
+task_list = ["Call John for AmI project", "Buy a new mouse", "Find a present for Anelina's birthday", "Organize mega party (last week of April)", "Book summer Holidays", "WhatsApp Mary"]
 print(sorted(task_list))
 index = int(input("1. Insert new task, 2. Remove task, 3. Show all existing tasks, 4. Close program, Choose an action 1-4: "))
-while index!= 4:
+while index != 4:
     if index == 1:
         new_list = input("What task would you like to add: ")
         task_list.append(new_list)
@@ -12,8 +12,6 @@ while index!= 4:
         task_list.remove(remove_list)
         index = int(input("1. Insert new task, 2. Remove task, 3. Show all existing tasks, 4. Close program, Choose an action 1-4: "))
     elif index == 3:
-        print(sorted(task_list))
+        task_list.sort()
+        print(task_list)
         index = int(input("1. Insert new task, 2. Remove task, 3. Show all existing tasks, 4. Close program, Choose an action 1-4: "))
-
-
-
